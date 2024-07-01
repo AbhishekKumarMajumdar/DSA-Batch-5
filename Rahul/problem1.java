a6e431d187b2a6c7420dd4bd9ab4533024f385f0
rahul7060 <rs0275645@gmail.com>
2024-07-01 23:31:13 +0530

    sollution1

---
+++ Rahul/problem1.java
@@ -0,0 +1,35 @@
+
+// Problem1
+
+package Rahul;
+
+
+import java.util.Scanner;
+
+public class problem1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String a = scanner.nextLine();
+        String b = scanner.nextLine();
+
+        // length of a and b
+        System.out.println(a.length() + " " +b.length());
+
+        // whole string
+        System.out.println(a+b);
+        
+        if (a.length() > 0 && b.length() >0) {
+            String aSwap = b.charAt(0)+a.substring(1); //string b ka 1st char + remaing a
+            String bSwap = a.charAt(0)+b.substring(1); //string a ka 1st char + remaing b           
+            
+            System.out.println(aSwap+ " "+bSwap);
+        }else {
+            System.out.println(a+" "+b); 
+            // if use gives 0value then input value print
+            //agar if statement nahi lagaya to thread aaiga
+        }
+        return;
+    }
+    
+}
