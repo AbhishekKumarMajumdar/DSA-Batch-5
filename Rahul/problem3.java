d44a52fc7838b3fe6222bca5afe1f4cceb325e87
rahul7060 <rs0275645@gmail.com>
2024-07-04 00:48:33 +0530

    sollution3

--- Rahul/problem3.java
+++ Rahul/problem3.java
@@ -5,49 +5,55 @@ import java.util.Scanner;
  */
 public class problem3 {
 
-    public boolean canPlaceFlower(int[] flowerbed, int n) {
+    public boolean canPlaceFlower(int [] flowerbed, int n ) {
         int max = 0;
 
-        int fi = -1; // first 1's index after 0's
-        int li = -1; // last 1's index before 0's
 
-        for (int i = 0; i < flowerbed.length; i++) { // corrected loop condition
-            if (flowerbed[i] == 0) {
-                continue;
-            } else if (fi == -1) { // if found first index 1
-                fi = i; // first index
-                li = i; // last index
-            } else { // now we have first 1th index now move li to find last 1th index
-                li = i;
-            } // now we have first and last 1th index
-        }
+       int fi = -1; // first 1's index after 0's
+       int li = -1; // last 1's index before 0's
 
-        // for all zeros in array
-        if (fi == -1) {
-            max = (flowerbed.length + 1) / 2; // if all zeros then..
-        } else { // if we found 1 first index of 1
-            int left = fi;
-            int right = flowerbed.length - 1 - li; // corrected calculation of right
+       for (int i = 0; i < flowerbed.length; i++){
+        if (flowerbed[i] == 0) {
+            continue;
+        } else if(fi == -1) { // if found first index 1
+            fi = i;  // first index
+            li = i; // last index
+        } else { // now we have first 1th index now move li to find last 1th index
+            li = i; 
+        }   /// now we have first and last 1th index
+       }
 
-            max = left / 2;
-            max += right / 2;
+       // for all zeros in array
 
-            int count = 0;
-            for (int i = fi + 1; i < li; i++) { // corrected loop condition
-                if (flowerbed[i] == 0) { // when we are finding 0s then counting going on
-                    count++;
-                } else { // we found 1
-                    max += (count - 1) / 2;
-                    count = 0;
-                }
+       if (fi == -1) { 
+        max = (flowerbed.length + 1) / 2; // if all zeros then..
+
+       } else { // if we found 1 first index of 1
+        int left = fi;
+        int right = flowerbed.length -1 - li; // right index of 1
+
+        max = left/2;  
+        max += right/2;
+
+        int count = 0;
+        for (int i= fi +1; i < li; i++) {
+
+            if (flowerbed[i] == 0) { //when we founding 0s then counting going on
+                count++;
+            } else { // we found 1
+                max += (count -1) / 2;
+                count = 0;
             }
-            max += (count - 1) / 2;
+            
+            
         }
+        max += (count - 1) / 2;
+
+       }
 
         return n <= max;
     }
 
-    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -68,4 +74,5 @@ public class problem3 {
 
         System.out.println("Can place flowers: " + result);
     }
+
 }
